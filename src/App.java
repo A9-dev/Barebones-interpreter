fbba27fdf4f4683510c18736be668a35031a95ca
A9-dev <henrypearson422@gmail.com>
2021-10-21 10:44:03 +0100

    Read lines from file, probably structured incorrectly

--- src/App.java
+++ src/App.java
@@ -1,5 +1,16 @@
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class App {
+    static String[] lines;
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String content = new String(Files.readAllBytes(Paths.get("barebonesCode.txt")));
+        System.out.println(content);
+        content = content.replace("\n", "").replace("\r", "");
+        lines = content.split(";");
+        for (int i = 0; i < lines.length; i++) {
+            System.out.println(lines[i]);
+        }
     }
 }
