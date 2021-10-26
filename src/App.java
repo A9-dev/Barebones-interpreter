fa5b51dc57ede5a62d3e464f69d25c4c704fcf36
A9-dev <henrypearson422@gmail.com>
2021-10-26 14:42:41 +0100

    Quite possible the worst code I've ever written but it works!!!!!!!!!!!!!!

--- src/App.java
+++ src/App.java
@@ -1,15 +1,12 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Stack;
 
 public class App {
     static Map<String, Integer> variables = new HashMap<String, Integer>();
-    Stack<String> whileLoopConStack = new Stack<String>();
 
     public static void main(String[] args) throws Exception {
         // Main function: extracts code from txt, splits into lines, parses lines,
@@ -23,31 +20,28 @@ public class App {
     private static String extracted() throws IOException {
         // Reads lines from txt and removes \n from every line
         String content = new String(Files.readAllBytes(Paths.get("barebonesCode.txt")));
-        content = content.replaceAll("^\s*-", "").replace("\r", "").replace("\n", "");
+        content = content.replace("\r", "").replace("\n", "").replaceAll(";\s*", ";");
 
-        System.out.println("roiejasopijseropij");
         System.out.println(content);
         return content;
     }
 
     public static String[][] parse(String[] lines) {
-        // Creating 2d array for opcode and operand per line
         String[][] linesSplit = new String[lines.length][1];
         for (int i = 0; i < lines.length; i++) {
-            System.out.println(String.format("Adding to 2d array: %s", Arrays.toString(lines[i].split(" "))));
-
             linesSplit[i] = lines[i].split(" ");
         }
-        System.out.println(String.format("Final array: %s", Arrays.deepToString(linesSplit)));
-
         return linesSplit;
     }
 
     private static void execute(String[][] code) {
         for (int i = 0; i < code.length; i++) {
             String opcode = code[i][0];
-            String operand = code[i][1];
-            ArrayList<String> cars = new ArrayList<String>();
+            String operand = "";
+
+            if (!opcode.equals("end")) {
+                operand = code[i][1];
+            }
 
             switch (opcode) {
             case "clear":
@@ -77,6 +71,36 @@ public class App {
             case "while":
                 try {
 
+                    Integer endLine = 0;
+                    while (variables.get(operand) != 0) {
+                        Integer depth = 0;
+                        for (int j = i + 1; j < code.length; j++) {
+                            if (code[j][0].equals("while")) {
+                                depth += 1;
+                            } else if (code[j][0].equals("end")) {
+                                if (depth == 0) {
+                                    endLine = j;
+
+                                } else {
+                                    depth -= 1;
+                                }
+                            }
+                        }
+                        String[][] newArray = Arrays.copyOfRange(code, i + 1, endLine);
+                        execute(newArray);
+
+                    }
+                    i = endLine;
+
+                } catch (Exception e) {
+                    System.err.println(e);
+
+                }
+
+                break;
+            case "end":
+                try {
+
                 } catch (Exception e) {
                     System.err.println(e);
                 }
@@ -85,6 +109,14 @@ public class App {
                 break;
             }
             System.out.println(variables.toString());
+            try {
+
+                if (variables.get("X") == -10) {
+                    System.exit(1);
+                }
+            } catch (Exception e) {
+
+            }
         }
     }
 }
